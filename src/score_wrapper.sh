2ca01398bd12a1a9bdcfa570c8a2c26902c2c610
Calvin Morrison <mutantturkey@gmail.com>
2014-03-25 13:51:39 -0400

    working upscore_wrapper.sh
    Score wrapper essentially does a few things
    
    1) backgrounds score_mers.py
    2) creates an intercept for any Ctrl+C
    3) waits for score_mers.py to finish
    4) if there's an intercept, send a SEGV to the process and try and exit
    
    it works, it might be dirty, you might see some output... but still at
    least you can exit now

--- src/score_wrapper.sh
+++ src/score_wrapper.sh
@@ -2,15 +2,17 @@
 
 
 function clean_exit {
-
-	kill -s 9 $pid
+	echo -e "\nInterrupt caught. Exiting cleanly..."
+	echo "killing $pid"
+	kill -11 $pid
+	sleep 1
 	exit
 }
-trap clean_exit SIGHUP SIGINT
+trap clean_exit SIGINT
 
 
 echo "score_mers.py $1 $2 $3 $4 &"
-score_mers.py $1 $2 $3 $4 
+score_mers.py $1 $2 $3 $4 & </dev/null
 pid=$!
 
 wait $pid
